d458904c7d7f1f8689d131246b529304ecec3abb
Tsung-Hua Tsai <tsai.tsunghua@gmail.com>
2016-09-05 15:00:29 +0800

    Update tests

--- tests/metrics/test_regression.py
+++ tests/metrics/test_regression.py
@@ -9,11 +9,10 @@ def test_average_accuracy():
 
     y_true = np.array([1.0, 1.0, 1.0])
     y_pred = np.array([1.1, 1.1, 1.1])
-    acc = average_accuracy(y_true, y_pred, full_range=1.0)
-    acc_true = 0.98999999999999999
+    acc = average_accuracy(y_true, y_pred, full_range=2.0)
+    acc_true = 0.995
      
-
-    assert np.abs(acc - acc_true) < 1e-8 
+    assert acc == acc_true
 
 
 if __name__ == '__main__':
---
+++ tests/preprocess/test_time_window.py
@@ -0,0 +1,23 @@
+import pytest
+from handpose.preprocess import window_method
+import numpy as np
+from numpy.testing import assert_allclose
+
+
+
+def test_window_method():
+    
+    num_rows = 3
+    num_cols = 2
+    num_elements = num_rows * num_cols
+    
+    a = np.array([i for i in range(num_elements)]).reshape((num_rows, num_cols))
+    b = window_method(a, win_size=1)
+    b_true = np.array([[0, 1, 2, 3], [2, 3, 4, 5]])  
+
+    assert_allclose(b, b_true)
+
+
+
+if __name__ == '__main__':
+    pytest.main([__file__])
