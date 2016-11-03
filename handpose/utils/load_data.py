1ac0873594edc449bef701c1d632ed1314f7a05e
Andrew Tsai <tsai.tsunghua@gmail.com>
2016-11-03 14:43:52 +0800

    Add tools to load and plot data.

--- handpose/utils/load_data.py
+++ handpose/utils/load_data.py
@@ -58,41 +58,50 @@ def csv2numpy(file_path):
     
     return arr
     
-def load_class_data(label_candidates, dir_path):
+def load_class_data(candidates, dir_path, equal_weight=True,  verbose=0):
     """
     Load the training data for classification.
     
     Parameters
     ----------
-    label_candidates: list
-        Label candidates.
+    candidates: list
+        Class candidates.
     
     dir_path: str
         Directory path.
-    
+   
+    equal_weight: bool
+        Equal weight for each class. 
+
+    verbose: int    
+        Switch used to show the debug information. 
+        Available value is 0, 1 or 2.
+
     Returns
     -------
     data: array-like
         Training data.
     """   
 
-    num_classes = len(label_candidates)
+    num_classes = len(candidates)
     
     class_files =  [[] for c in range(num_classes)]
     class_data =   [[] for c in range(num_classes)]
     class_labels = [[] for c in range(num_classes)]
   
     for c in range(num_classes): 
-        filenames = find_class_files(label_candidates[c], dir_path)
+        filenames = find_class_files(candidates[c], dir_path)
         class_files[c].append(filenames)
         
         # Prepare class data
         for filename in filenames: 
             file_path = "{}/{}".format(dir_path, filename)
-            print("Reading the file: {}".format(file_path))
+            if verbose > 1:
+                print("Reading the file: {}".format(file_path))
             arr = csv2numpy(file_path)
             arr = arr[:, 1:] # Remove the timestamp
-            print("The shape of data is {}".format(arr.shape))
+            if verbose > 1:
+                print("The shape of data is {}".format(arr.shape))
             class_data[c].append(arr)
 
         class_data[c] = np.concatenate(class_data[c]) 
@@ -100,20 +109,66 @@ def load_class_data(label_candidates, dir_path):
         # Prepare the class labels
         class_labels[c] = np.full(len(class_data[c]), c, dtype=np.int32)
 
-        print("--------")
-        print("There are {} instances in class {}".format(len(class_labels[c]), c))
-        print("--------")
+        if verbose > 0:
+            print("--------")
+            print("There are {} instances in class {} (labeled as {})".format(len(class_labels[c]), candidates[c], c))
 
     # Truncate instances for equal weighting
-    num_min_instances = 100000000
-    for c in range(num_classes):
-        num_instances = len(class_data[c])
-        if num_min_instances < num_instances:
-            num_min_instances = num_instances
+    if equal_weight:
+        num_min = 100000000
+        for c in range(num_classes):
+            num_instances = len(class_data[c])
+            if num_min > num_instances:
+                num_min = num_instances
+
+        for c in range(num_classes):
+            class_data[c] = class_data[c][:num_min, :]
+            class_labels[c] = class_labels[c][:num_min]
+
+        if verbose > 0:
+            print("--------")
+            print("For equal weight, each class uses {} instances".format(num_min))
     
     # Concatenate data and labels
     data = np.concatenate(class_data) 
     labels = np.concatenate(class_labels)
+
+    if verbose > 0:
+        num_instances, num_features = data.shape
+        print("--------")
+        print("Instance number is {}".format(num_instances))
+        print("Feature number is {}".format(num_features))
         
     return data, labels
 
+
+def load_csv_file(file_path, start_col=1):
+    """
+    Load a CSV file and output as a numpy array.
+    
+    Parameters
+    ----------
+    file_path: str
+        Path of the CSV file.
+    
+    start_col: int
+        Starting column index. default is 1 because index 0 is for timestamp.
+
+    Returns
+    -------
+    arr: numpy array
+        Data array.
+    """  
+
+    # Read the CSV file as dataframe
+    df  = pd.read_csv(file_path)
+    arr = df.values
+
+    # Set the data type as float
+    arr = arr.astype(np.float32)
+
+    # Skip the timestamp
+    arr = arr[:, start_col:]
+    
+    return arr
+    
--- handpose/utils/plot_utils.py
+++ handpose/utils/plot_utils.py
@@ -2,8 +2,46 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+def plot_imp_x(imps, frame_index, frame_shift, class_name, 
+               subjects=None):
+    """
+    Plot impedance in spatial space.
 
-def plot_imp_hist(df, tx=0, rx=1):
+    Parameters
+    ----------
+    imps: list
+        List of impedance arrays.
+    frame_index: int
+        Frame index.
+    frame_shift: int
+        Shift frames.
+    class_name: str
+        Class name.
+    subjects: list
+        List of subject's name.
+
+    """  
+    num_imps = len(imps)
+
+    if subjects == None:
+        subjects = []
+        for i in range(num_imps):
+             subjects.append("subject{}".format(i+1))
+
+    rows, cols = imps[0].shape
+    x = range(cols)
+    
+    fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(12, 3))
+
+    for i in range(num_imps):
+        axes[i].plot(x, imps[i][frame_index], x, imps[i][frame_index+frame_shift])
+        axes[i].set_title('class: {}, {}'.format(class_name, subjects[i]))
+        axes[i].set_xlabel('electrod index')
+        axes[i].set_ylabel('impedances')
+    
+    return fig, axes
+
+def plot_imp_t(df, tx=0, rx=1):
     """
     Plot impedance values including real and imaginary parts.
 
@@ -29,4 +67,5 @@ def plot_imp_hist(df, tx=0, rx=1):
   
     df[[col_name1, col_name2]].plot(ax=axes[1])
     axes[1].set_xlabel('time index')
+
 
