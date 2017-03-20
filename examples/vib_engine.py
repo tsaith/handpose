cae5c10b3664cb27435eaddc2b5e5aadbbf5c9c1
Andrew Tsai <tsai.tsunghua@gmail.com>
2017-03-20 15:34:38 +0800

    Update example

--- examples/vib_engine.py
+++ examples/vib_engine.py
@@ -1,4 +1,5 @@
 import numpy as np
+from handpose.vib import has_gesture
 from handpose.engine import EngineConfig, VibEngine
 
 # Model path
@@ -21,10 +22,14 @@ num_dims = 3
 num_features = fs*num_dims
 
 X_test = np.zeros(num_features)
+
+gesture_exists = has_gesture(X_test)
+
 X_test = X_test[np.newaxis,:]
 
 [proba_pred] = engine.predict_proba(X_test)
 
+print("Has a gesture? {}".format(gesture_exists))
 print("Probability information:")
 for c in range(len(proba_pred)):
     print("    class: {}, proba: {}".format(c, proba_pred[c]))
