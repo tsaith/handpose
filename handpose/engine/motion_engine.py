b4a46f6d72affada2c7a6a3c774fc34f5a63dcde
Andrew Tsai <tsai.tsunghua@gmail.com>
2017-04-06 14:36:30 +0800

    Set the critical angle as 30 degree.

--- handpose/engine/motion_engine.py
+++ handpose/engine/motion_engine.py
@@ -28,7 +28,7 @@ def get_motion_class(theta, phi):
         0, 1, 2, 3, 4 -> static, up, down, left, right
     """
 
-    angle_c = np.pi / 18.0 # Critical angle
+    angle_c = 3.0* np.pi/18.0 # Critical angle, 30 degree as default
 
     theta_a = theta[0]
     theta_z = theta[-1:][0]
