1c76e04d2e4e78590a90e3e9ce0def7c9b0e8264
Andrew Tsai <tsai.tsunghua@gmail.com>
2017-06-20 11:52:14 +0800

    Show probabilities of normalized confusion matrix.

--- handpose/utils/confusion_matrix.py
+++ handpose/utils/confusion_matrix.py
@@ -37,7 +37,7 @@ def plot_confusion_matrix(cm, classes,
     tick_marks = np.arange(len(classes))
     plt.xticks(tick_marks, classes, rotation=45)
     plt.yticks(tick_marks, classes)
-    
+
     if normalize:
         cm = normalize_confusion_matrix(cm)
         print("Normalized confusion matrix")
@@ -46,7 +46,7 @@ def plot_confusion_matrix(cm, classes,
 
     thresh = cm.max() / 2.
     for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        plt.text(j, i, cm[i, j],
+        plt.text(j, i, "{:3.2f}".format(cm[i, j]),
                  horizontalalignment="center",
                  color="white" if cm[i, j] > thresh else "black")
 
