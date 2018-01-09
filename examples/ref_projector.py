8418265848b63f05584a9a913e80d0b1e8ace7eb
Andrew Tsai <tsai.tsunghua@gmail.com>
2018-01-09 15:14:11 +0800

    Add RefProjector() to obtained the projected vector on the reference planes.

---
+++ examples/ref_projector.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+from handpose.sensor_fusion import *
+from handpose.utils import *
+from handpose.tracking import *
+
+
+vec0 = np.array([1, 0, 0])
+
+# Make first rotation
+theta = 0.0 / 180 * np.pi
+phi = 30.0 / 180 *np.pi
+q_ref = Quaternion.from_spherical(theta, phi)
+
+# Construct a reference projector
+projector = RefProjector()
+
+# Make second rotation
+theta = 0.0 / 180 * np.pi
+phi = 30.0 / 180 *np.pi
+q_ref = Quaternion.from_spherical(theta, phi)
+
+# Vector representation in the reference axes
+vec_ref = q_ref.rotate_vector(vec0)
+
+projector.q_ref = q_ref
+theta = 0.0 / 180 * np.pi
+phi = -60.0 / 180 *np.pi
+q_target = Quaternion.from_spherical(theta, phi)
+
+# Vector representation in the target axes
+vec_target = q_target.rotate_vector(vec0)
+
+# Projected vector
+vec_proj = projector.project_vector(vec_target)
+
+print("vec0 = {}".format(vec0))
+print("vec_ref = {}".format(vec_ref))
+print("vec_target = {}".format(vec_target))
+print("vec_proj = {}".format(vec_proj))
